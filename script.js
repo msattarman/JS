60cbf9884e5325058c2c4d62e482a9b62e9b4874
msatt <msattarman@gmail.com>
2022-04-04 20:32:17 +0500

    complete lesson02

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+// Lesson02 Типы данных, операторы, методы, свойства. 
 
-alert('Hello all');
+let title = 'JS';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 3;
+let rollback = 25;
+let fullPrice = 1000000;
+let adaptive = true;
 
-console.log('Welcome');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log("Длина строки screens = " + screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log( screens.toLocaleLowerCase().split(' '));
+console.log(`Процент отката посреднику за работу ${rollback}%, ${fullPrice * (rollback/100)}`);
