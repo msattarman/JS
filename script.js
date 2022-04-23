86401220ddc7cbd5814588e7622279b8c0870087
msatt <msattarman@gmail.com>
2022-04-24 00:34:36 +0500

    Complete lesson03

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-// Lesson02 Типы данных, операторы, методы, свойства. 
+// Lesson03 Ветвления. 
+'use strict';
 
-const title = 'JS';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 3;
-const rollback = 25;
-const fullPrice = 1000000;
-const adaptive = true;
+const title = prompt('Как называется ваш проект?'); 
+console.log('Название проекта: [', title,'] '); 
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+const screens = prompt('Какие типы экранов нужно разработать? \n Перечислите через запятую');
+console.log('Необходимо разработать типы экранов: [', screens,']'); 
 
-console.log("Длина строки screens = " + screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
-console.log( screens.toLocaleLowerCase().split(' '));
-console.log(`Процент отката посреднику за работу ${rollback}%, ${fullPrice * (rollback/100)}`);
+const screenPrice = prompt('Сколько будет стоить данная работа?');
+console.log(`Работа будет стоить [${screenPrice} рублей]`);
+ 
+const adaptive = confirm('Нужен ли адаптив?');
+console.log(`Адаптив: [${adaptive ? 'нужен' : 'не нужен'}]`);
+
+let service1 = prompt('Какой дополнительный тип услуги №1 нужен?');
+
+let servicePrice1 = prompt(`Сколько будет стоить ${service1}?`);
+console.log(`${service1} - ${servicePrice1} рублей`);
+
+let service2 = prompt('Какой дополнительный тип услуги №2 нужен?');
+
+let servicePrice2 = prompt(`Сколько будет стоить ${service2}?`);
+console.log(`${service2} - ${servicePrice2} рублей`);
+
+const fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+console.log(`Итоговая стоимость работы - ${fullPrice}`);
+
+const rollback = 10;  // % отката посреднику
+console.log(`Откат посреднику от стоимости заказа - ${rollback}% `);
+let servicePercentPrice = Math.ceil( fullPrice * (100-rollback) / 100 );
+console.log(`ДОХОД разработчика - ${servicePercentPrice} рублей`);
+
+if ( fullPrice >= 30000 ) { 
+  console.log('Даем скидку в 10%'); 
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log('Даем скидку в 5%');
+} else if (fullPrice > 0 && fullPrice < 15000 ) {
+  console.log('Скидка не предусмотрена');
+} else {
+  console.log('Что то пошло не так...'); 
+}
