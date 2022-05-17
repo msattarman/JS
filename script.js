247219c871758fbb4ccce2ac1041adb5db9ac3ba
msatt <msattarman@gmail.com>
2022-05-17 18:23:27 +0500

    clean code

--- script.js
+++ script.js
@@ -2,34 +2,16 @@
 'use strict';
 
 const title = prompt('Как называется ваш проект?'); 
-console.log('Название проекта: [', title,'] '); 
-
-const screens = prompt('Какие типы экранов нужно разработать? \n Перечислите через запятую');
-console.log('Необходимо разработать типы экранов: [', screens,']'); 
-
-const screenPrice = prompt('Сколько будет стоить данная работа?');
-console.log(`Работа будет стоить [${screenPrice} рублей]`);
- 
+const screens = prompt('Какие типы экранов нужно разработать?\nПеречислите через запятую');
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
 const adaptive = confirm('Нужен ли адаптив?');
-console.log(`Адаптив: [${adaptive ? 'нужен' : 'не нужен'}]`);
-
 let service1 = prompt('Какой дополнительный тип услуги №1 нужен?');
-
-let servicePrice1 = prompt(`Сколько будет стоить ${service1}?`);
-console.log(`${service1} - ${servicePrice1} рублей`);
-
+let servicePrice1 = +prompt(`Сколько будет стоить ${service1}?`);
 let service2 = prompt('Какой дополнительный тип услуги №2 нужен?');
-
-let servicePrice2 = prompt(`Сколько будет стоить ${service2}?`);
-console.log(`${service2} - ${servicePrice2} рублей`);
-
+let servicePrice2 = +prompt(`Сколько будет стоить ${service2}?`);
 const fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-console.log(`Итоговая стоимость работы - ${fullPrice}`);
-
 const rollback = 10;  // % отката посреднику
-console.log(`Откат посреднику от стоимости заказа - ${rollback}% `);
-let servicePercentPrice = Math.ceil( fullPrice * (100-rollback) / 100 );
-console.log(`ДОХОД разработчика - ${servicePercentPrice} рублей`);
+let servicePercentPrice = Math.ceil( fullPrice - (fullPrice * (rollback / 100)));
 
 if ( fullPrice >= 30000 ) { 
   console.log('Даем скидку в 10%'); 
@@ -40,3 +22,13 @@ if ( fullPrice >= 30000 ) {
 } else {
   console.log('Что то пошло не так...'); 
 }
+
+console.log(`Название проекта [${title}]`); 
+console.log(`Необходимо разработать типы экранов: [${screens}]`); 
+console.log(`Работа будет стоить [${screenPrice} рублей]`);
+console.log(`Адаптив: [${adaptive ? 'нужен' : 'не нужен'}]`);
+console.log(`${service1} - ${servicePrice1} рублей`);
+console.log(`${service2} - ${servicePrice2} рублей`);
+console.log(`Итоговая стоимость работы - [${fullPrice}]`);
+console.log(`Доход разработчика - [${servicePercentPrice}] рублей`);
+console.log(`Откат посреднику от стоимости заказа - [${rollback}%]`);
