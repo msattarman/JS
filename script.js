7809a380acf6dc820d63037b3b4759e1ac15f84c
msatt <msattarman@gmail.com>
2022-05-17 18:09:27 +0500

    add let

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-// Lesson03 Ветвления. 
 'use strict';
 
-const title = prompt('Как называется ваш проект?'); 
-console.log('Название проекта: [', title,'] '); 
-
-const screens = prompt('Какие типы экранов нужно разработать? \n Перечислите через запятую');
-console.log('Необходимо разработать типы экранов: [', screens,']'); 
-
-const screenPrice = prompt('Сколько будет стоить данная работа?');
-console.log(`Работа будет стоить [${screenPrice} рублей]`);
- 
-const adaptive = confirm('Нужен ли адаптив?');
-console.log(`Адаптив: [${adaptive ? 'нужен' : 'не нужен'}]`);
-
-let service1 = prompt('Какой дополнительный тип услуги №1 нужен?');
-
-let servicePrice1 = prompt(`Сколько будет стоить ${service1}?`);
-console.log(`${service1} - ${servicePrice1} рублей`);
-
-let service2 = prompt('Какой дополнительный тип услуги №2 нужен?');
-
-let servicePrice2 = prompt(`Сколько будет стоить ${service2}?`);
-console.log(`${service2} - ${servicePrice2} рублей`);
-
-const fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-console.log(`Итоговая стоимость работы - ${fullPrice}`);
-
-const rollback = 10;  // % отката посреднику
-console.log(`Откат посреднику от стоимости заказа - ${rollback}% `);
-let servicePercentPrice = Math.ceil( fullPrice * (100-rollback) / 100 );
-console.log(`ДОХОД разработчика - ${servicePercentPrice} рублей`);
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги №1 нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let rollback = 10;  
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = (fullPrice - (fullPrice * (rollback / 100)));
 
 if ( fullPrice >= 30000 ) { 
   console.log('Даем скидку в 10%'); 
@@ -40,3 +21,12 @@ if ( fullPrice >= 30000 ) {
 } else {
   console.log('Что то пошло не так...'); 
 }
+
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
